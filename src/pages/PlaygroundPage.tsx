7dc1aaafc06f2922f5f9453b7e165b03478c1f85
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-06-13 11:21:48 +0530

    Testing the ExpandableText component

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,7 @@
-import TermsAndConditions from "../components/TermsAndConditions";
+import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return <ExpandableText text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, debitis. Excepturi eius impedit autem eligendi sit quis. Assumenda porro expedita exercitationem ex aut impedit ipsam placeat est, similique perspiciatis, perferendis consectetur obcaecati deleniti aspernatur nisi laudantium illum culpa aliquam maiores non excepturi nostrum? Vitae exercitationem repellendus, explicabo rem voluptatibus veniam! Molestias corporis laudantium animi architecto illo. Velit rem non repudiandae inventore a doloremque voluptates. Quisquam debitis commodi accusantium illum incidunt deleniti aspernatur doloribus dolorem explicabo consectetur. Delectus laborum fugiat eum vel quas, pariatur, soluta animi cumque repellendus officiis officia ducimus libero necessitatibus ex sed illum voluptatibus enim excepturi similique blanditiis?" />;
 };
 
 export default PlaygroundPage;
